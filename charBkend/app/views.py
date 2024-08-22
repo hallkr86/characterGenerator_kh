b56228e8efd5b387eb5770dc731a85475a779636
Kesha Hall <k_renee10@yahoo.com>
2024-08-22 10:42:54 -0400

    added my views, urls, and settings

--- charBkend/app/views.py
+++ charBkend/app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+# from rest_framework.views import APIView
+from . models import *
+from . serializer import *
+# from rest_framework.response import Response
+
+from rest_framework import generics
 
 # Create your views here.
+# class ReactView(APIView):
+#     def get(self, request):
+#         output = [{"character": output.character, "power": output.power}
+        
+#         for output in React.objects.all()]
+#         return Response(output)
+
+
+#     def post(self, request):
+#         serializer = ReactSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data)
+
+class ReactView(generics.ListCreateAPIView):
+    queryset = React.objects.all()
+    serializer_class = ReactSerializer
