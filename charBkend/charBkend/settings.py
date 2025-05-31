e25bc40e309ad36ce902f0dd3583a2ca003170ee
Kesha Hall <k_renee10@yahoo.com>
2025-05-31 11:30:39 -0400

    changed allowed host

--- charBkend/charBkend/settings.py
+++ charBkend/charBkend/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('charactergenerator-kh.onrender.com')
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
     
